94feefe3eeb20f375a87559ed94cd75bbbf735c7
ninak23 <80950548+ninak23@users.noreply.github.com>
2021-05-15 14:22:57 +0200

    update

--- Aufgabe2.4/script.js
+++ Aufgabe2.4/script.js
@@ -3,6 +3,9 @@ var Aufgabe2_4;
 (function (Aufgabe2_4) {
     let parts2 = JSON.parse(Aufgabe2_4.partsJson);
     let imgIds = [parts2.Kopfbedeckungen[0].Image, parts2.Oberteile[0].Image, parts2.Hosen[0].Image, parts2.Schuhe[0].Image];
+    let aktuelleSeite = window.location.href;
+    let pos = aktuelleSeite.lastIndexOf("/");
+    aktuelleSeite = aktuelleSeite.substring(pos + 1);
     function writeStorage(_part) {
         sessionStorage.setItem(_part.Image, _part.DateiName);
     }
@@ -11,6 +14,7 @@ var Aufgabe2_4;
         for (let i = 0; i < imgIds.length; i++) {
             let div = document.createElement("img");
             div.setAttribute("src", sessionStorage.getItem(imgIds[i]));
+            div.setAttribute("alt", "Noch kein Artikel ausgewählt");
             div.id = "kopfbedeckung";
             prevElement.appendChild(div);
         }
@@ -35,9 +39,6 @@ var Aufgabe2_4;
         btn.innerText = "Select";
         // Möglichkeit 1: innere Funktion
         btn.addEventListener("click", handleSelection);
-        // Möglichkeit 2: äußere Funktion 
-        btn.addEventListener("click", handleSelection2);
-        btn.dataset.index = _index.toString();
         div.appendChild(btn);
         return div;
         // innere Funktion, welche Durch ihre Positionierung innerhalb der createPartDiv Funktion das _part noch kennt. Darum kann man einfach folgendes machen:
@@ -45,15 +46,33 @@ var Aufgabe2_4;
             console.log("innere Funktion", _part);
             writeStorage(_part);
             console.log(sessionStorage.getItem(_part.Image));
+            let prevElement = document.body;
+            let div = document.createElement("img");
+            //prevElement.removeChild(div);
+            div.setAttribute("src", sessionStorage.getItem(_part.Image));
+            div.id = "test";
+            prevElement.appendChild(div);
+            let _aktuelleSeite = window.location.href;
+            let _pos = _aktuelleSeite.lastIndexOf("/");
+            _aktuelleSeite = _aktuelleSeite.substring(_pos + 1);
+            switch (_aktuelleSeite) {
+                case "Index.html":
+                    showSelection();
+                    break;
+                case "Index2.html":
+                    window.location.href = "Index3.html";
+                    break;
+                case "Index3.html":
+                    window.location.href = "Index4.html";
+                    break;
+                case "Index4.html":
+                    window.location.href = "Index5.html";
+                    break;
+                case "Index5.html":
+                    window.location.href = "Index.html";
+                    break;
+            }
         }
-    }
-    // äußere Funktion, welche nun anderweitig herausfinden muss, welchen Part wir gewählt haben.
-    // in diesem Fall habe ich den index im heads Array auf dem Button im dataset hinterlegt.
-    // Da der Button das ist, was das Event auslößt, können wir über _e.currentTarget darauf zugreifen.
-    function handleSelection2(_e) {
-        let target = _e.currentTarget;
-        let index = Number(target.dataset.index);
-        console.log("äußere Funktion", parts2.Kopfbedeckungen[index]);
     }
     function showPossibilities(_parts) {
         let wrapper = document.getElementById("selectionWrapper");
@@ -62,23 +81,66 @@ var Aufgabe2_4;
             wrapper.appendChild(div);
         }
     }
-    let aktuelleSeite = window.location.href;
-    let pos = aktuelleSeite.lastIndexOf("/");
-    aktuelleSeite = aktuelleSeite.substring(pos + 1);
-    if (aktuelleSeite == "Index.html") {
-        showSelection();
-    }
-    if (aktuelleSeite == "Index2.html") {
-        showPossibilities(parts2.Kopfbedeckungen);
-    }
-    if (aktuelleSeite == "Index3.html") {
-        showPossibilities(parts2.Oberteile);
-    }
-    if (aktuelleSeite == "Index4.html") {
-        showPossibilities(parts2.Hosen);
-    }
-    if (aktuelleSeite == "Index5.html") {
-        showPossibilities(parts2.Schuhe);
+    switch (aktuelleSeite) {
+        case "Index.html":
+            showSelection();
+            break;
+        case "Index2.html":
+            showPossibilities(parts2.Kopfbedeckungen);
+            let prevElement = document.body;
+            let div = document.createElement("img");
+            div.setAttribute("src", sessionStorage.getItem(parts2.Kopfbedeckungen[0].Image));
+            div.id = "test";
+            prevElement.appendChild(div);
+            break;
+        case "Index3.html":
+            showPossibilities(parts2.Oberteile);
+            let prevElement2 = document.body;
+            let div2 = document.createElement("img");
+            div2.setAttribute("src", sessionStorage.getItem(parts2.Kopfbedeckungen[0].Image));
+            div2.id = "test";
+            prevElement2.appendChild(div2);
+            let div3 = document.createElement("img");
+            div3.setAttribute("src", sessionStorage.getItem(parts2.Oberteile[0].Image));
+            div3.id = "test";
+            prevElement2.appendChild(div3);
+            break;
+        case "Index4.html":
+            showPossibilities(parts2.Hosen);
+            let prevElement3 = document.body;
+            let div4 = document.createElement("img");
+            div4.setAttribute("src", sessionStorage.getItem(parts2.Kopfbedeckungen[0].Image));
+            div4.id = "test";
+            prevElement3.appendChild(div4);
+            let div5 = document.createElement("img");
+            div5.setAttribute("src", sessionStorage.getItem(parts2.Oberteile[0].Image));
+            div5.id = "test";
+            prevElement3.appendChild(div5);
+            let div6 = document.createElement("img");
+            div6.setAttribute("src", sessionStorage.getItem(parts2.Hosen[0].Image));
+            div6.id = "test";
+            prevElement3.appendChild(div6);
+            break;
+        case "Index5.html":
+            showPossibilities(parts2.Schuhe);
+            let prevElement4 = document.body;
+            let div7 = document.createElement("img");
+            div7.setAttribute("src", sessionStorage.getItem(parts2.Kopfbedeckungen[0].Image));
+            div7.id = "test";
+            prevElement4.appendChild(div7);
+            let div8 = document.createElement("img");
+            div8.setAttribute("src", sessionStorage.getItem(parts2.Oberteile[0].Image));
+            div8.id = "test";
+            prevElement4.appendChild(div8);
+            let div9 = document.createElement("img");
+            div9.setAttribute("src", sessionStorage.getItem(parts2.Hosen[0].Image));
+            div9.id = "test";
+            prevElement4.appendChild(div9);
+            let div10 = document.createElement("img");
+            div10.setAttribute("src", sessionStorage.getItem(parts2.Schuhe[0].Image));
+            div10.id = "test";
+            prevElement4.appendChild(div10);
+            break;
     }
 })(Aufgabe2_4 || (Aufgabe2_4 = {}));
 //# sourceMappingURL=script.js.map
--- Aufgabe2.4/script.ts
+++ Aufgabe2.4/script.ts
@@ -1,105 +1,180 @@
-
 namespace Aufgabe2_4 {
 
-    let parts2: Outfit = JSON.parse(partsJson);
-    let imgIds: string[] = [parts2.Kopfbedeckungen[0].Image, parts2.Oberteile[0].Image, parts2.Hosen[0].Image, parts2.Schuhe[0].Image];
-  
-    function writeStorage(_part: Kleidungsstueck): void {
-      sessionStorage.setItem(_part.Image, _part.DateiName);
+  let parts2: Outfit = JSON.parse(partsJson);
+  let imgIds: string[] = [parts2.Kopfbedeckungen[0].Image, parts2.Oberteile[0].Image, parts2.Hosen[0].Image, parts2.Schuhe[0].Image];
+
+  let aktuelleSeite: string = window.location.href;
+  let pos: number = aktuelleSeite.lastIndexOf("/");
+  aktuelleSeite = aktuelleSeite.substring(pos + 1);
+
+  function writeStorage(_part: Kleidungsstueck): void {
+    sessionStorage.setItem(_part.Image, _part.DateiName);
+  }
+
+  function showSelection(): void {
+    let prevElement: HTMLElement = document.body;
+    for (let i: number = 0; i < imgIds.length; i++) {
+      let div: HTMLDivElement = document.createElement("img");
+      div.setAttribute("src", sessionStorage.getItem(imgIds[i]));
+      div.setAttribute("alt", "Noch kein Artikel ausgewählt");
+      div.id = "kopfbedeckung";
+      prevElement.appendChild(div);
     }
-  
-    function showSelection(): void {
+
+  }
+
+  function createPartDiv(_part: Kleidungsstueck, _index: number): HTMLDivElement {
+    // wrapping div
+    let div: HTMLDivElement = document.createElement("div");
+    div.classList.add("kleidungsstueck");
+
+    // image to be displayed
+    let img: HTMLImageElement = document.createElement("img");
+    img.src = _part.DateiName;
+    div.appendChild(img);
+
+    // description
+
+    let span: HTMLSpanElement = document.createElement("span");
+    span.innerText = _part.Typ;
+    div.appendChild(span);
+
+    //let preis: HTMLSpanElement = document.createElement("preis");
+    //preis.innerHTML = _part.Preis;
+    //div.appendChild(preis);
+
+    // button
+    let btn: HTMLButtonElement = document.createElement("button");
+    btn.innerText = "Select";
+    // Möglichkeit 1: innere Funktion
+    btn.addEventListener("click", handleSelection);
+
+    div.appendChild(btn);
+
+    return div;
+
+    // innere Funktion, welche Durch ihre Positionierung innerhalb der createPartDiv Funktion das _part noch kennt. Darum kann man einfach folgendes machen:
+    function handleSelection(_e: Event): void {
+      console.log("innere Funktion", _part);
+      writeStorage(_part);
+      console.log(sessionStorage.getItem(_part.Image));
+
       let prevElement: HTMLElement = document.body;
-      for (let i: number = 0; i < imgIds.length; i++) {
-        let div: HTMLDivElement = document.createElement("img");
-        div.setAttribute("src", sessionStorage.getItem(imgIds[i]));
-        div.id = "kopfbedeckung";
-        prevElement.appendChild(div);
-        }
-      
-    }
-  
-    function createPartDiv(_part: Kleidungsstueck, _index: number): HTMLDivElement {
-      // wrapping div
-      let div: HTMLDivElement = document.createElement("div");
-      div.classList.add("kleidungsstueck");
-  
-      // image to be displayed
-      let img: HTMLImageElement = document.createElement("img");
-      img.src = _part.DateiName;
-      div.appendChild(img);
-  
-      // description
-  
-      let span: HTMLSpanElement = document.createElement("span");
-      span.innerText = _part.Typ;
-      div.appendChild(span);
-  
-      //let preis: HTMLSpanElement = document.createElement("preis");
-      //preis.innerHTML = _part.Preis;
-      //div.appendChild(preis);
-  
-      // button
-      let btn: HTMLButtonElement = document.createElement("button");
-      btn.innerText = "Select";
-      // Möglichkeit 1: innere Funktion
-      btn.addEventListener("click", handleSelection);
-  
-      // Möglichkeit 2: äußere Funktion 
-      btn.addEventListener("click", handleSelection2);
-      btn.dataset.index = _index.toString();
-  
-      div.appendChild(btn);
-  
-      return div;
-  
-      // innere Funktion, welche Durch ihre Positionierung innerhalb der createPartDiv Funktion das _part noch kennt. Darum kann man einfach folgendes machen:
-      function handleSelection(_e: Event): void {
-        console.log("innere Funktion", _part);
-        writeStorage(_part);
-        console.log(sessionStorage.getItem(_part.Image));
+      let div: HTMLDivElement = document.createElement("img");
+      //prevElement.removeChild(div);
+      div.setAttribute("src", sessionStorage.getItem(_part.Image));
+      div.id = "test";
+      prevElement.appendChild(div);
+      let _aktuelleSeite: string = window.location.href;
+      let _pos: number = _aktuelleSeite.lastIndexOf("/");
+      _aktuelleSeite = _aktuelleSeite.substring(_pos + 1);
+      switch (_aktuelleSeite) {
+        case "Index.html":
+          showSelection();
+          break;
+
+        case "Index2.html":
+          window.location.href = "Index3.html";
+          break;
+
+        case "Index3.html":
+          window.location.href = "Index4.html";
+          break;
+
+        case "Index4.html":
+          window.location.href = "Index5.html";
+          break;
+
+        case "Index5.html":
+          window.location.href = "Index.html";
+          break;
+
       }
     }
-  
-    // äußere Funktion, welche nun anderweitig herausfinden muss, welchen Part wir gewählt haben.
-    // in diesem Fall habe ich den index im heads Array auf dem Button im dataset hinterlegt.
-    // Da der Button das ist, was das Event auslößt, können wir über _e.currentTarget darauf zugreifen.
-    function handleSelection2(_e: Event): void {
-      let target: HTMLElement = <HTMLElement> _e.currentTarget;
-      let index: number = Number(target.dataset.index);
-  
-      console.log("äußere Funktion", parts2.Kopfbedeckungen[index]);
-  
+  }
+
+  function showPossibilities(_parts: Kleidungsstueck[]): void {
+    let wrapper: HTMLDivElement = <HTMLDivElement>document.getElementById("selectionWrapper");
+    for (let i: number = 0; i < _parts.length; i++) {
+      let div: HTMLDivElement = createPartDiv(_parts[i], i);
+      wrapper.appendChild(div);
     }
-  
-    function showPossibilities(_parts: Kleidungsstueck[]): void {
-      let wrapper: HTMLDivElement = <HTMLDivElement> document.getElementById("selectionWrapper");
-      for (let i: number = 0; i < _parts.length; i++) {
-        let div: HTMLDivElement = createPartDiv(_parts[i], i);
-        wrapper.appendChild(div);
-      }
-    }
-    
-    let aktuelleSeite: string = window.location.href;
-    let pos: number = aktuelleSeite.lastIndexOf("/");
-    aktuelleSeite = aktuelleSeite.substring(pos + 1);
-    if (aktuelleSeite == "Index.html") {
+  }
+
+  switch (aktuelleSeite) {
+    case "Index.html":
       showSelection();
-  }
-    if (aktuelleSeite == "Index2.html") {
+      break;
+
+    case "Index2.html":
       showPossibilities(parts2.Kopfbedeckungen);
-  }
-    if (aktuelleSeite == "Index3.html") {
+      let prevElement: HTMLElement = document.body;
+      let div: HTMLDivElement = document.createElement("img");
+      div.setAttribute("src", sessionStorage.getItem(parts2.Kopfbedeckungen[0].Image));
+      div.id = "test";
+      prevElement.appendChild(div);
+      break;
+
+    case "Index3.html":
       showPossibilities(parts2.Oberteile);
-  }
-    if (aktuelleSeite == "Index4.html") {
+      let prevElement2: HTMLElement = document.body;
+      let div2: HTMLDivElement = document.createElement("img");
+      div2.setAttribute("src", sessionStorage.getItem(parts2.Kopfbedeckungen[0].Image));
+      div2.id = "test";
+      prevElement2.appendChild(div2);
+
+      let div3: HTMLDivElement = document.createElement("img");
+      div3.setAttribute("src", sessionStorage.getItem(parts2.Oberteile[0].Image));
+      div3.id = "test";
+      prevElement2.appendChild(div3);
+      break;
+
+    case "Index4.html":
       showPossibilities(parts2.Hosen);
-  }
-  
-    if (aktuelleSeite == "Index5.html") {
+      let prevElement3: HTMLElement = document.body;
+      let div4: HTMLDivElement = document.createElement("img");
+      div4.setAttribute("src", sessionStorage.getItem(parts2.Kopfbedeckungen[0].Image));
+      div4.id = "test";
+      prevElement3.appendChild(div4);
+
+      let div5: HTMLDivElement = document.createElement("img");
+      div5.setAttribute("src", sessionStorage.getItem(parts2.Oberteile[0].Image));
+      div5.id = "test";
+      prevElement3.appendChild(div5);
+
+      let div6: HTMLDivElement = document.createElement("img");
+      div6.setAttribute("src", sessionStorage.getItem(parts2.Hosen[0].Image));
+      div6.id = "test";
+      prevElement3.appendChild(div6);
+      break;
+
+    case "Index5.html":
       showPossibilities(parts2.Schuhe);
+      let prevElement4: HTMLElement = document.body;
+      let div7: HTMLDivElement = document.createElement("img");
+      div7.setAttribute("src", sessionStorage.getItem(parts2.Kopfbedeckungen[0].Image));
+      div7.id = "test";
+      prevElement4.appendChild(div7);
+
+      let div8: HTMLDivElement = document.createElement("img");
+      div8.setAttribute("src", sessionStorage.getItem(parts2.Oberteile[0].Image));
+      div8.id = "test";
+      prevElement4.appendChild(div8);
+
+      let div9: HTMLDivElement = document.createElement("img");
+      div9.setAttribute("src", sessionStorage.getItem(parts2.Hosen[0].Image));
+      div9.id = "test";
+      prevElement4.appendChild(div9);
+
+      let div10: HTMLDivElement = document.createElement("img");
+      div10.setAttribute("src", sessionStorage.getItem(parts2.Schuhe[0].Image));
+      div10.id = "test";
+      prevElement4.appendChild(div10);
+      break;
+
   }
-  
-  
-  
-  }
+}
+
+
+
+
