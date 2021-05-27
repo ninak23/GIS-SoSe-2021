a6f28dfae1f0973a85001c13b269e46a6b6d4780
ninak23 <80950548+ninak23@users.noreply.github.com>
2021-05-27 10:14:52 +0200

    update

--- Aufgabe3.1/test.js
+++ Aufgabe3.1/test.js
@@ -1,14 +1,9 @@
 "use strict";
 console.log("hallo Node JS Server");
+test();
 function test() {
     let x = 10;
     let a = ["hallo", "gh", "sdf"];
     console.log(a[1], x);
 }
-test();
-//let formData: FormData = new FormData(document.forms[0]);
-/**let url: string = "https://whatever.server/path/file";
-let query: URLSearchParams = new URLSearchParams(<any>formData);
-url = url + "?" + query.toString();
-await fetch(url);*/ 
 //# sourceMappingURL=test.js.map
--- Aufgabe3.1/test.ts
+++ Aufgabe3.1/test.ts
@@ -1,4 +1,7 @@
+
+
 console.log("hallo Node JS Server");
+test();
 
 function test(): void {
     let x: number = 10;
@@ -7,12 +10,5 @@ function test(): void {
     console.log(a[1], x);
 }
 
-test();
 
 
-//let formData: FormData = new FormData(document.forms[0]);
-
-/**let url: string = "https://whatever.server/path/file";
-let query: URLSearchParams = new URLSearchParams(<any>formData);
-url = url + "?" + query.toString();
-await fetch(url);*/
