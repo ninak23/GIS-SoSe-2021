91e8b3ae7182ac71f1ebd1d4ee30b7f40ddad60b
ninak23 <80950548+ninak23@users.noreply.github.com>
2021-05-30 15:36:20 +0200

    Update test.ts

--- Aufgabe3.1/test.ts
+++ Aufgabe3.1/test.ts
@@ -1,20 +1,18 @@
 
+document.getElementsByTagName("button")[0].addEventListener("click", Server);
+
+async function Server(): Promise<void> {
+
+    let formData: FormData = new FormData(document.forms[0]);
+    let url: string = "https://ninakgissose2020.herokuapp.com/";
+    let query: URLSearchParams = new URLSearchParams(<any>formData);
+    url = url + "?" + query.toString();
+    let response: Response = await fetch(url);
+    let answer: string = await response.text();
+    console.log(answer);
 
 
-    document.getElementsByTagName("button")[0].addEventListener("click", Server);
-
-    async function Server(): Promise<void> {
-
-        let formData: FormData = new FormData(document.forms[0]);
-        let url: string = "https://ninakgissose2020.herokuapp.com/";
-        let query: URLSearchParams = new URLSearchParams(<any>formData);
-        url = url + "?" + query.toString();
-        let response: Response = await fetch(url);
-        let answer: string = await response.text();
-        console.log(answer);
-
-
-    }
+}
 
 
 
