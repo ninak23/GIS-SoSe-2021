c3b9d9211c3e12194a5f28dcba433022f7853e94
ninak23 <80950548+ninak23@users.noreply.github.com>
2021-07-06 21:07:52 +0200

    update

--- Abschlussaufgabe - Kopie/client/client.ts
+++ Abschlussaufgabe - Kopie/client/client.ts
@@ -9,7 +9,8 @@ namespace Client {
     }
 
 
-    let serverURL: string = "https://mongodbnetbrowser.herokuapp.com/" ; 
+    //let serverURL: string = "https://mongodbnetbrowser.herokuapp.com/" ; 
+    let serverURL: string = "https://ninakgissose2020.herokuapp.com/";
 
     document.getElementById("insertButton")?.addEventListener("click", input);
     //document.getElementById("responseButton")?.addEventListener("click", getData);
